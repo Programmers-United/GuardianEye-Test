0b44f330917e2e93baa359aa9272eba957236a71
Douglas Silva <douglassilvaaraujo78@gmail.com>
2023-10-01 20:46:31 -0300

    refactor: Creating a notification system and validating the form

--- frontend/src/scripts/newEvent.js
+++ frontend/src/scripts/newEvent.js
@@ -1,15 +1,20 @@
 /* Arquivo para trabalhar com as informações recebidas através do formulário */
 const form = document.getElementById("occurrence-form");
+const notification = document.querySelector('.notification');
 
+const confirm = document.getElementById("confirm");
+
+//Envio do formulário aqui:
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    /** Dados vindo do formulário */
     const title = document.getElementById("Title").value; 
     const descr = document.getElementById("Description").value;
     const inputRadio = document.querySelector('input[name="crime-type"]:checked');
     const date = document.getElementById("Data").value; 
     const time = document.getElementById("Time").value; 
 
-    if (title.length > 0) { //Verificação de informação verdadeira mais rígida (Melhorar)
+    if (checkFields()) { 
         const createObject = { //Criando objeto com as informações(Pode ser últil na adição do elemento ao banco de dados)
             title,
             descr,
@@ -17,9 +22,80 @@ form.addEventListener("submit", (e) => {
             date,
             time
         };
-
         console.log(createObject);
     }
-
-    alert("Tentativa de adicionar"); //Criar um elemento e uma função para notificar a adição
+    customizedNotification(title);
 });
+
+//Função para validar o formulário (Falta a localização)
+const checkFields = () =>{
+    // Selecionar todos os campos do formulário
+    const campos = document.querySelectorAll('.input-details');
+    // Verificar se algum campo está vazio
+    for (const campo of campos) {
+        if (campo.value.trim() === '') {
+            // Se algum campo estiver vazio, retorne false
+            return false;
+        }
+    }
+    // Verificar se um dos radio buttons foi selecionado
+    const radioButtons = document.querySelectorAll('input[name="crime-type"]');
+    let radioButtonSelecionado = false;
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            radioButtonSelecionado = true;
+            break;
+        }
+    }
+    // Verificar se pelo menos um radio button foi selecionado
+    if (!radioButtonSelecionado) {
+        return false;
+    }
+    // Se todos os campos estiverem preenchidos, retorne true
+    return true;
+}
+
+/** Função para customizar a notificação */
+const customizedNotification = (title) =>{
+    //Dados da notificação:
+    const navTitle = document.querySelector(".nav-title");
+    const navName = document.querySelector(".nav-name");
+    const navMenssage = document.querySelector(".nav-message");
+    const percursor = document.querySelector(".percursor");
+    const notification = document.querySelector(".notification");
+
+    // Mostrar a notificação
+    notification.style.display = 'block';
+    confirm.disabled = true;
+
+    if(checkFields()){
+        navTitle.textContent = "Ocorrência Inserida";
+        navName.textContent = `Título: ${title}`;
+        navMenssage.textContent = "Ocorrência adicionada com sucesso!";
+        percursor.style.backgroundColor = "var(--aquaGreen)";
+        notification.style.backgroundColor = "var(--white)";
+        clearFields();
+    }else{
+        navTitle.textContent = "Ocorrência não Inserida";
+        navName.textContent = "Erro na inserção";
+        navMenssage.textContent = "Verifique os campos do formulário.";
+        percursor.style.backgroundColor = "var(--red)";
+        notification.style.backgroundColor = "var(--rosee)";
+    }
+     // Aguarde 10 segundos e oculte completamente a notificação
+     setTimeout(() => {
+        notification.style.display = 'none';
+        confirm.disabled = false;
+     }, 6000); // Tempo em milissegundos (6 segundos)
+}
+
+/** Limpar os campos caso tenha acertado */
+const clearFields = () =>{
+    // Selecione todos os campos de entrada dentro do formulário
+    const inputFields = form.querySelectorAll('input');
+
+    // Verifique se todos os campos estão preenchidos
+    for (const inputField of inputFields) {
+        inputField.value = "";
+    }
+}
