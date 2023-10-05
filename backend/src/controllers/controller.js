3df404d60bce36180eac85d20e041f0780a9e265
Douglas Silva <douglassilvaaraujo78@gmail.com>
2023-10-05 13:59:32 -0300

    add: Modulating the API methods

---
+++ backend/src/controllers/controller.js
@@ -0,0 +1,35 @@
+const Point = require("../modal/point");
+
+//Method for listing occurrences
+module.exports.listOccurrences = async function (req, res){
+    const point = await Point.findAll();
+    res.status(200).send(point);
+};
+
+//Method for adding occurrences
+module.exports.addOccurrences = async function (req, res){
+    const point = Point.build(req.body);
+    try{
+        await point.save();
+        res.status(202).send("Saved");
+    }catch(err){
+        console.log(err);
+        res.status(400).send("Falid save");
+    }
+};
+
+//Method for deleting occurrences
+module.exports.delOccurrences = async function (req, res){
+    try{
+        const deletados = await Point.destroy({
+          where: { id : req.params.id } });
+    
+        if(deletados > 0){
+          res.status(200).send('Occurrence removed');
+        }else{
+          res.status(404).send('Occurrence not found');
+        }
+      }catch(err){
+        res.status(400).send('Failed to delete');
+      }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,27 +1,14 @@
-const dotonev = require('dotenv');
-dotonev.config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const route = require('./routes/route');
+const dotonev = require('dotenv');
+dotonev.config();
+
 app.use(express.json());
-
-const Point = require("./modal/point");
-
-app.post("/point", async function (req, res){
-    const point = Point.build(req.body);
-    try{
-        await point.save();
-        res.status(202).send("Saved");
-    }catch(err){
-        console.log(err);
-        res.status(400).send("Falid save");
-    }
-});
-
-app.get("/point", async function (req, res){
-    const point = await Point.findAll();
-    res.status(200).send(point);
-});
+app.use(cors());
+app.use('/point', route);
 
 app.listen(process.env.API_PORT, ()=>{
     console.log("Servidor rodando na porta "+ process.env.API_PORT);
-})
+});
