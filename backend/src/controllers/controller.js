3f0db6b11b40894ed92c214d3085337c1a81f420
Douglas Silva <douglassilvaaraujo78@gmail.com>
2023-12-06 11:13:09 -0300

    refactor: Updating the Neo4j access method

--- backend/src/controllers/controller.js
+++ backend/src/controllers/controller.js
@@ -11,8 +11,8 @@ function haversineDistance(lat1, lon1, lat2, lon2) {
   const dLon = toRadians(lon2 - lon1);
 
   const a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
 
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
@@ -27,13 +27,13 @@ function toRadians(degrees) {
 }
 
 //Method for listing occurrences
-module.exports.listOccurrences = async function (req, res){
-    const point = await Point.find({});
-    res.status(200).send(point);
+module.exports.listOccurrences = async function (req, res) {
+  const point = await Point.find({});
+  res.status(200).send(point);
 };
 
 //Method for adding occurrences
-module.exports.addOccurrences = async function (req, res){
+module.exports.addOccurrences = async function (req, res) {
   try {
     //Criando o ponto
     const point = await Point.create(req.body);
@@ -42,25 +42,28 @@ module.exports.addOccurrences = async function (req, res){
     const coordinates = point.geometric.coordinates;
 
     //Salvando o ponto como objeto como nó no neo4j
-    await neo4jController.salvar(point.title, point.type, coordinates);
+    await neo4jController.salvar(point.id, point.title, point.type, coordinates);
 
     //Criando relação de tipo
-    await neo4jController.relationType(point.title);
+    await neo4jController.relationType(point.id);
 
     // Obtendo todos os pontos existentes
     const allPoints = await Point.find({});
 
     // Verificando a distância entre o novo ponto e todos os outros pontos
     for (const existingPoint of allPoints) {
-        const existingCoordinates = existingPoint.geometric.coordinates;
-        const distance = haversineDistance(coordinates[1], coordinates[0], existingCoordinates[1], existingCoordinates[0]);
+      const existingCoordinates = existingPoint.geometric.coordinates;
+      const distance = haversineDistance(coordinates[1], coordinates[0], existingCoordinates[1], existingCoordinates[0]);
 
-        if (distance <= 2 && existingPoint.title != point.title) {
-            // Criando relacionamento e armazenando a distância
-            await neo4jController.relationDistance(point.title, existingPoint.title, distance);
-        }
+      if (distance <= 2 && existingPoint.id != point.id) {
+        // Criando relacionamento e armazenando a distância
+        await neo4jController.relationDistance(point.id, existingPoint.id, distance);
+      }
     }
 
+    //Pode remover o controller do neo4j de upload, pode funcinar assim: Antes de atualizar, remove e depois cria outro nó, podendo assim criar os relacionamentos
+    //Em relação ao relacionamento, pode haver um if, onde a função só será chamada se houver um nó já armazenado
+    //Em relação ao relacionamento de proximidade, para sempre garantir que haverá um relacionamento, pode fazer o relacionamento em duas vetentes, onde: A -> B & B -> A
     res.status(202).send(point);
   } catch (error) {
     if (error.code === 11000 && error.keyPattern.title) {
@@ -74,7 +77,7 @@ module.exports.addOccurrences = async function (req, res){
 };
 
 //Method for deleting occurrences
-module.exports.delOccurrences = async function (req, res){
+module.exports.delOccurrences = async function (req, res) {
   try {
     const eventId = req.params.id;
 
@@ -89,9 +92,9 @@ module.exports.delOccurrences = async function (req, res){
     if (!point) {
       return res.status(404).json({ erro: "Ocorrência não encontrada" });
     }
-    
+
     //Removendo nó
-    await neo4jController.remove(point.title);
+    await neo4jController.remove(point.id);
 
     // Excluir o evento
     await Point.findByIdAndDelete(eventId);
@@ -105,7 +108,7 @@ module.exports.delOccurrences = async function (req, res){
 };
 
 //Method for updating occurrences
-module.exports.updateOccurrences = async function (req, res){
+module.exports.updateOccurrences = async function (req, res) {
   try {
     const eventId = req.params.id;
 
@@ -121,17 +124,41 @@ module.exports.updateOccurrences = async function (req, res){
       return res.status(404).json({ erro: "Evento não encontrado" });
     }
 
-    //Pegando as coordenadas do ponto atualizado
-    const coordinates = point.geometric.coordinates;
-
     //Atualizando nó
-    await neo4jController.update(point.title, point.type, coordinates);
+    //Primeiro remove o nó
+    await neo4jController.remove(point.id);
 
+    
     //Atualizando a ocorrência
-    const occorrenceUpdate = await Point.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    const occorrenceUpdate = await Point.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    
+    //Pegando as coordenadas do ponto atualizado
+    const coordinates = occorrenceUpdate.geometric.coordinates;
+    
+    //Segundo cria o nó com todas as informações e relacionamentos atualizadas
+    //Salvando o ponto como objeto como nó no neo4j
+    await neo4jController.salvar(occorrenceUpdate.id, occorrenceUpdate.title, occorrenceUpdate.type, coordinates);
+
+    //Criando relação de tipo
+    await neo4jController.relationType(point.id);
+
+    // Obtendo todos os pontos existentes
+    const allPoints = await Point.find({});
+
+    // Verificando a distância entre o novo ponto e todos os outros pontos
+    for (const existingPoint of allPoints) {
+      const existingCoordinates = existingPoint.geometric.coordinates;
+      const distance = haversineDistance(coordinates[1], coordinates[0], existingCoordinates[1], existingCoordinates[0]);
+
+      if (distance <= 2 && existingPoint.id != point.id) {
+        // Criando relacionamento e armazenando a distância
+        await neo4jController.relationDistance(point.id, existingPoint.id, distance);
+      }
+    }
+
     res.status(200).send(occorrenceUpdate);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ erro: "Erro interno do servidor" });   
+    res.status(500).json({ erro: "Erro interno do servidor" });
   }
 }
